252bd107c188d479734ab160f5f6a7c3a38defa4
takayuki-s <taka12yuki04@gmail.com>
2021-04-01 14:24:45 +0900

    ActiveHashによるグループ選択

---
+++ app/models/group.rb
@@ -0,0 +1,19 @@
+class Group < ActiveHash::Base
+  self.data = [
+    { id: 1, name: 'Aグループ'},
+    { id: 2, name: 'Bグループ'},
+    { id: 3, name: 'Cグループ'},
+    { id: 4, name: 'Dグループ'},
+    { id: 5, name: 'Eグループ'},
+    { id: 6, name: 'Fグループ'},
+    { id: 7, name: 'Gグループ'},
+    { id: 8, name: 'Hグループ'},
+    { id: 9, name: 'Iグループ'},
+    { id: 10, name: 'Jグループ'},
+    { id: 11, name: 'Kグループ'},
+    { id: 12, name: 'Lグループ'},
+    { id: 13, name: 'Mグループ'},
+    { id: 14, name: 'Nグループ'},
+  ]
+
+end
