2365c0e634dcf51e3a6afa25a3578386ce05c5e0
takayuki-s <taka12yuki04@gmail.com>
2021-04-01 21:36:05 +0900

    新規room作成画面修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,7 @@
 class Room < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :group
+
+  validates :name, presence: true
+  validates :group_id, presence: true
 end
