cc2e1a360731dee8400ec71e8db60bc31e2b90fe
takayuki-s <taka12yuki04@gmail.com>
2021-04-02 19:30:22 +0900

    アソシエーション記述

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :group
+  has_many :messages
 
   validates :name, presence: true
   validates :group_id, presence: true
